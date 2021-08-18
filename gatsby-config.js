5615f57a8b1e00dfe9a7efc7b0a25065014c4f25
lifeisatomato <86894892+lifeisatomato@users.noreply.github.com>
2021-08-18 00:06:33 -0400

    add insta link to description-omg

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,11 +8,11 @@ module.exports = {
   siteMetadata: {
     title: "Wai Recipes",
     shortTitle: "Recipes",
-    description: "Original recipes you’ll want to keep making.", 
+    description: "Original recipes you’ll want to keep making. Check out our IG: https://www.instagram.com/wairecipes/ if you like what you see here (❁´◡`❁).",
     url: "https://recipes-demo.marinda.me", // No trailing slash allowed!
     image: "/images/og-image.jpg", // Path to your image you placed in the 'static' folder
     author: "",
-    intro: "Classic recipes you’ll want to keep making. Check out our IG page <a href="https://www.instagram.com/wairecipes/" target="_blank"  rel="noreferrer noopener">@wairecipes</a> if you like what you see here (❁´◡`❁).",
+    intro: "Original recipes you’ll want to keep making. Check out our IG: https://www.instagram.com/wairecipes/ if you like what you see here (❁´◡`❁).",
     menuLinks: [
       {
         name: "About",
